eceee1d05e9505a1adb6a1fcc56d2b888b2bf9f3
shahzebAM <76158680+shahzebAM@users.noreply.github.com>
2021-06-10 05:11:59 -0700

    Update List.js

--- src/List.js
+++ src/List.js
@@ -1,57 +1,49 @@
 import React from "react";
-const List = [{
-    name: "Episode 60",
-    src: "//ok.ru/videoembed/2313365359230",
-    link: "#"
+const List = [
+    {
+    name: "Episode 62",
+    src: "//ok.ru/videoembed/2344081427070",
+    link: "https://ok6-9.vkuser.net/?expires=1623413495498&srcIp=39.52.131.235&srcAg=CHROME&ms=95.142.206.136&type=3&sig=8hLWJir733g&ct=0&urls=45.136.22.21%3B185.226.52.75&clientType=1&zs=43&id=1167183907454"
 },
 
 {
     name: "Episode 61",
-    src: "https://www.youtube.com/embed/VmEU22NYkEs",
-    link: "Download link",
+    src: "//ok.ru/videoembed/2328592910974",
+    link: "#"
+},
+    {
+    name: "Episode 60",
+    src: "//ok.ru/videoembed/2313365359230",
+    link: "#"
+},
+    {
+    name: "Episode 59",
+    src: "//ok.ru/videoembed/2453640383171",
+    link: "#"
+},
+        {
+    name: "Episode 58",
+    src: "//ok.ru/videoembed/2440330152643",
+    link: "#"
+},
+        {
+    name: "Episode 57",
+    src: "//ok.ru/videoembed/2926053820952",
+    link: "#"
+},
+        {
+    name: "Episode 56",
+    src: "//ok.ru/videoembed/2250081176190",
+    link: "#"
+},
+        {
+    name: "Episode 55",
+    src: "//ok.ru/videoembed/2890852076056",
+    link: "#"
+},
     
-},
 
-{
-    name: "Episode 62",
-    src: "https://www.youtube.com/embed/raYnzbIyOfE",
-    link: "Download link",
-},
-{
-    name: "Episode 62",
-    src: "https://www.youtube.com/embed/VmEU22NYkEs",
-    link: "Download link",
-},
-{
-    name: "Episode 62",
-    src: "https://www.youtube.com/embed/VmEU22NYkEs",
-    link: "Download link",
-},
-{
-    name: "Episode 67",
-    src: "https://www.youtube.com/embed/VmEU22NYkEs",
-    link: "Download link",
-},
-{
-    name: "Episode 67",
-    src: "https://www.youtube.com/embed/VmEU22NYkEs",
-    link: "Download link",
-},
-{
-    name: "Episode 67",
-    src: "https://www.youtube.com/embed/VmEU22NYkEs",
-    link: "Download link",
-},
-{
-    name: "Episode 69",
-    src: "https://www.youtube.com/embed/VmEU22NYkEs",
-    link: "Download link",
-},
-{
-    name: "Episode 69",
-    src: "https://www.youtube.com/embed/VmEU22NYkEs",
-    link: "Download link",
-},
+
 
 
 
