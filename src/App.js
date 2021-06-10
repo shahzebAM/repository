219c40e3087fd1640bc5cf01b0300152874117d9
Shahzeb Ali <shahzebali654321@gmail.com>
2021-06-10 11:34:37 -0700

    add netlify folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import list from "./List";
+import React from "react";
+import { Navbar,Container,Card,Row,Col } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import List from './List';
+import { Button } from '@material-ui/core';
+
+const Fun = (val) => {
+  return(
+    <>
+
+
+<div className="css">
+
+    <Card style={{ width: '18rem' }}>
+    {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+    <Card.Body style={{padding:10}}>
+      <Card.Title className="title" style={{fontSize: 18,fontFamily:'sans', fontWeight: 'bold'}}>{val.name}</Card.Title>
+      {/* <Card.Text> */}
+      <iframe width="200" height="150" src={val.src} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      {/* </Card.Text> */}
+      {/* <Button className="btn">Go somewhere</Button> */}
+      <Button variant="outlined" href={val.link} style={{marginLeft: 45, height: 30}} className="btn">
+  Download
+</Button>
+    </Card.Body>
+  </Card>
+</div>
+   
+
+
+
+
+    </>
+  )
+
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar className="nav" fixed="top" expand="lg" variant="light" bg="light">
+  <Container>
+    <Navbar.Brand href="#" >Kurulus Osman</Navbar.Brand>
+  </Container>
+</Navbar>
+{List.map(Fun)}
+    </>
   );
 }
 
---
+++ src/List.js
@@ -0,0 +1,39 @@
+import React from "react";
+const List = [{
+    name: "Episode 60",
+    src: "https://www.youtube.com/embed/VmEU22NYkEs",
+    link: "Download link"
+},
+
+{
+    name: "Episode 61",
+    src: "https://www.youtube.com/embed/VmEU22NYkEs",
+    link: "Download link",
+    
+},
+
+{
+    name: "Episode 62",
+    src: "https://www.youtube.com/embed/raYnzbIyOfE",
+    link: "Download link",
+},
+{
+    name: "Episode 62",
+    src: "https://www.youtube.com/embed/VmEU22NYkEs",
+    link: "Download link",
+},
+{
+    name: "Episode 62",
+    src: "https://www.youtube.com/embed/VmEU22NYkEs",
+    link: "Download link",
+},
+
+
+
+
+
+
+
+]
+
+export default List;
